126f15e45d415864aaf61d35813d759b30fa7638
Sarobidy-23 <hei.sarobidy@gmail.com>
2023-03-01 19:12:54 +0300

    chore: extract all type to new folder

---
+++ src/type/typeUtils.ts
@@ -0,0 +1,12 @@
+export type EnvPropsType = {
+    caseSize: number
+}
+export type PositionType = {
+    x: number,
+    y: number
+}
+
+export type RewardIndexedType = {
+    index: PositionType,
+    score: number
+}
